712947f1eea601c72ade15abcf12e941bd17fce3
yukihiro-kawabata <yukihirochasuke0630@gmail.com>
2020-05-05 16:59:36 +0900

    Slack通知を環境設定ファイルでコントロールできるようにした

--- app/Model/slack/slack_push_model.php
+++ app/Model/slack/slack_push_model.php
@@ -13,23 +13,23 @@ class slack_push_model extends Model
      */ 
     public function push_msg(string $msg = '') : void
     {
-
         $url = Config::get('slack.api_url');
+        if (!empty($url)) {
+            // 送信メッセージ。
+            $data['text'] = $msg;
+            
+            // curlを初期化
+            $ch = curl_init();
 
-        // 送信メッセージ。
-        $data['text'] = $msg;
-        
-        // curlを初期化
-        $ch = curl_init();
+            // 設定!
+            curl_setopt($ch, CURLOPT_URL, $url); // 送り先
+            curl_setopt($ch, CURLOPT_POST, true); // POSTですS
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data)); // 送信データ
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // 実行結果取得の設定
 
-        // 設定!
-        curl_setopt($ch, CURLOPT_URL, $url); // 送り先
-        curl_setopt($ch, CURLOPT_POST, true); // POSTですS
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data)); // 送信データ
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // 実行結果取得の設定
-
-        curl_exec($ch);
-        curl_close($ch);
+            curl_exec($ch);
+            curl_close($ch);
+        }
     }
 
 }
